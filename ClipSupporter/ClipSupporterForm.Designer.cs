de3c11a65fdb4715f1e60a47cdc5a6a8829b6bf7
konuma <k.koyama1985@gmail.com>
2019-09-24 22:34:09 +0900

    first version

---
+++ ClipSupporter/ClipSupporterForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ClipSupporter
+{
+    partial class ClipSupporterForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClipSupporterForm));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageKC = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPageCalc = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPageKC.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "a";
+            this.notifyIcon1.BalloonTipTitle = "b";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageKC);
+            this.tabControl1.Controls.Add(this.tabPageCalc);
+            this.tabControl1.Location = new System.Drawing.Point(4, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(240, 65);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPageKC
+            // 
+            this.tabPageKC.Controls.Add(this.button2);
+            this.tabPageKC.Controls.Add(this.button1);
+            this.tabPageKC.Location = new System.Drawing.Point(4, 22);
+            this.tabPageKC.Name = "tabPageKC";
+            this.tabPageKC.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageKC.Size = new System.Drawing.Size(232, 39);
+            this.tabPageKC.TabIndex = 0;
+            this.tabPageKC.Text = "KC";
+            this.tabPageKC.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(114, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // tabPageCalc
+            // 
+            this.tabPageCalc.Location = new System.Drawing.Point(4, 22);
+            this.tabPageCalc.Name = "tabPageCalc";
+            this.tabPageCalc.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageCalc.Size = new System.Drawing.Size(232, 39);
+            this.tabPageCalc.TabIndex = 1;
+            this.tabPageCalc.Text = "Calc";
+            this.tabPageCalc.UseVisualStyleBackColor = true;
+            // 
+            // ClipSupporterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(248, 72);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.Name = "ClipSupporterForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "ClipSupporter";
+            this.TopMost = true;
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageKC.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageKC;
+        private System.Windows.Forms.TabPage tabPageCalc;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ ClipSupporter/ClipSupporterForm.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipSupporter
+{
+    public partial class ClipSupporterForm : Form
+    {
+        public string ClipDataFolderPath = String.Empty;
+        const string DATATYPE_TEXT = "text";
+        const string DATATYPE_BIN = "bin";
+        const string DATATYPE_FILE = "file";
+        readonly string[][] ChoiceFormatPriorities = {
+            new string[]{DataFormats.FileDrop, DATATYPE_FILE },
+            new string[]{DataFormats.EnhancedMetafile, DATATYPE_TEXT },
+            new string[]{DataFormats.OemText, DATATYPE_TEXT },
+            new string[]{DataFormats.Html, DATATYPE_TEXT },
+            new string[]{DataFormats.Bitmap, DATATYPE_BIN },
+            new string[]{DataFormats.UnicodeText, DATATYPE_TEXT },
+            new string[]{DataFormats.CommaSeparatedValue, DATATYPE_TEXT },
+            new string[]{DataFormats.Text, DATATYPE_TEXT },
+        };
+        enum DataSaveType
+        {
+            Text,
+            File,
+            Binary,
+        }
+
+        public ClipSupporterForm()
+        {
+            InitializeComponent();
+
+            // 初期設定の取り込み
+            ClipDataFolderPath = Path.Combine(ConfigurationManager.AppSettings["BasePath"]
+                , ConfigurationManager.AppSettings["AppName"]);
+
+            Directory.CreateDirectory(ClipDataFolderPath);
+            // clear folder and children files
+            ClearDataDirectory();
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var dataObj = Clipboard.GetDataObject();
+            if (dataObj == null) return;
+
+            string dataType = string.Empty;
+            string dataFormat = ChoiceFormat(dataObj.GetFormats(), ref dataType);
+            if (dataFormat == string.Empty) return;
+
+            // clear folder amd children files
+            ClearDataDirectory();
+
+            //MessageBox.Show(dataFormat + "\r\n" + dataObj.GetData(dataFormat));
+            Console.WriteLine(dataFormat + "\r\n" + dataObj.GetData(dataFormat));
+            string outputFilePath = Path.Combine(ClipDataFolderPath, dataFormat);
+
+            this.Text = "Save中...";
+            // Formatに合わせて保存
+            switch (dataType)
+            {
+                case DATATYPE_BIN:
+                    var img = Clipboard.GetImage();
+                    img.Save(outputFilePath);
+                    break;
+                case DATATYPE_FILE:
+                    if (!CopyDropFile((string[])dataObj.GetData(dataFormat)))
+                    {
+                        notifyIcon1.ShowBalloonTip(1000, "エラー", $"ファイルコピーに失敗しました。", ToolTipIcon.Info);
+                        ClearDataDirectory();
+                    }
+                    break;
+                case DATATYPE_TEXT:
+                    using (var sw = new StreamWriter(outputFilePath))
+                    {
+                        sw.Write(dataObj.GetData(dataFormat));
+                    }
+                    break;
+                default:
+                    notifyIcon1.ShowBalloonTip(1000, "情報", $"コピーしているデータには対応しておりません。{dataFormat}", ToolTipIcon.Info);
+                    return;
+            }
+            this.Text = "ClipSupporter";
+
+            // 動作確認の為、クリア
+            // Clipboard.Clear();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            var inputFileList = Directory.EnumerateFileSystemEntries(ClipDataFolderPath).ToArray();
+            string dataFormat = string.Empty;
+            string dataType = string.Empty;
+            if (inputFileList.Length > 1 
+                || (dataFormat = ChoiceFormat(inputFileList.Select(f => Path.GetFileName(f)).ToArray()
+                                             , ref dataType)) == string.Empty)
+            {
+                // Fileコピーの場合
+                Clipboard.SetData(DataFormats.FileDrop, inputFileList);
+            }
+            else
+            {
+                switch (dataType)
+                {
+                    case DATATYPE_BIN:
+                        using (var ms = new MemoryStream(File.ReadAllBytes(inputFileList[0])))
+                        {
+                            Bitmap bmp = new Bitmap(ms);
+                            Clipboard.SetData(dataFormat, bmp);
+                        }
+                        break;
+                    case DATATYPE_TEXT:
+                        using (var sr = new StreamReader(inputFileList[0]))
+                        {
+                            Clipboard.SetData(dataFormat, sr.ReadToEnd());
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+        private string ChoiceFormat(string[] formats, ref string dataType)
+        {
+            foreach (var fmt in formats)
+            {
+                if (ChoiceFormatPriorities.Any(f => f[0] == fmt))
+                {
+                    var hitFormat = ChoiceFormatPriorities.Where(f => f[0] == fmt).First();
+                    dataType = hitFormat[1];
+                    return hitFormat[0];
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private void ClearDataDirectory()
+        {
+            foreach (var item in Directory.EnumerateFiles(ClipDataFolderPath))
+            {
+                File.Delete(item);
+            }
+
+            foreach (var dir in Directory.EnumerateDirectories(ClipDataFolderPath))
+            {
+                Directory.Delete(dir, true);
+            }
+        }
+
+        private bool CopyDropFile(string[] dropFileArray)
+        {
+            foreach (var path in dropFileArray)
+            {
+                //ファイルをコピー
+                if (File.Exists(path))
+                {
+                    File.Copy(path, Path.Combine(ClipDataFolderPath, Path.GetFileName(path)));
+                }
+                else
+                {
+                    CopyAndReplace(path, Path.Combine(ClipDataFolderPath, Path.GetFileName(path)));
+                }
+            }
+
+            if (Directory.EnumerateFileSystemEntries(ClipDataFolderPath).Count()
+                == dropFileArray.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// ディレクトリとその中身を上書きコピー
+        /// </summary>
+        public void CopyAndReplace(string sourcePath, string copyPath)
+        {
+            Directory.CreateDirectory(copyPath);
+
+            //ファイルをコピー
+            foreach (var file in Directory.GetFiles(sourcePath))
+            {
+                File.Copy(file, Path.Combine(copyPath, Path.GetFileName(file)));
+            }
+
+            //ディレクトリの中のディレクトリも再帰的にコピー
+            foreach (var dir in Directory.GetDirectories(sourcePath))
+            {
+                CopyAndReplace(dir, Path.Combine(copyPath, Path.GetFileName(dir)));
+            }
+        }
+    }
+}
